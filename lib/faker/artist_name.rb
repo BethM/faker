e470fcf27b3792abf2a0b28490e285ef902662c7
Kim Godard <kgodard@comverge.com>
2012-12-21 10:20:39 -0700

    Make ArtistName generation random

--- lib/faker/artist_name.rb
+++ lib/faker/artist_name.rb
@@ -5,8 +5,12 @@ module Faker
     class << self
 
       def artist_name
-        fetch('artist_name.name')
+        parse('artist_name.name')
+      rescue
+        parse('artist_name.name')
       end
+
+      def part; fetch('artist_name.part'); end
     end
   end
 end
